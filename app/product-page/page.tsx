d0320160ad8ab878b1dba984b9ba5c800cd70f35
ALHFBK <albert0915a@gmail.com>
2024-11-18 01:48:32 +0800

    made title change according to product title

--- app/product-page/page.tsx
+++ app/product-page/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../model/product';
 import { useRouter } from 'next/navigation';
-import { SlActionUndo } from "react-icons/sl";
+import { SlActionUndo } from 'react-icons/sl';
 
 // const getProductFromLocalStorage = () => {
 //     if (typeof window !== 'undefined') {
@@ -34,22 +34,27 @@ export default function ProductContent(){
             setProduct(JSON.parse(storedProduct));
         }
     }, []);
+    useEffect(()=>{
+        if(product?.title){
+            document.title = product.title;
+        }
+    }, [product]);
 
     if (!product) return <p>Loading...</p>;
     return (
-        <div className="flex h-[100%] w-[100%] items-center pt-[10vh]">
+        <div className="flex h-[100%] w-[100%] pt-[10vh]">
             <div className="fixed top-10 left-10">
                 <button type="button" onClick={()=>router.back()} className="hover:opacity-70">
                     <SlActionUndo size={40}/>
                 </button>
             </div>
-            <div className="flex-col basis-1/2 pl-[15vw] pr-[2vw]">
-                <img src={product.image} className="h-[70vh] rounded-lg"/>
+            <div className="flex basis-1/2 h-[75vh] items-center justify-end">
+                <img src={product.image} className="rounded-lg max-h-[75vh] min-h-[50vh] pr-3"/>
             </div>
-            <div className="flex-col basis-1/2 pr-[10vw]">
+            <div className="flex-col basis-1/2 items-center">
                 <h1 className="text-[4em] font-bold">{product.title}</h1>
                 <div className="text-[3em] text-red-700 font-bold">{product.price} <span className="text-[0.5em]">元</span> </div>
-                <div>{product.description}</div>
+                <div className="max-w-[40vw]">{product.description}</div>
                 <button className="bg-red-700 w-[10em] h-[2em] text-white mt-[5em] ml-[3em] hover:opacity-60">add to cart</button>
             </div>
         </div>
