1486ed6cfda08bc973105e1e7aed8a982bbce031
Ateto1202 <tonysu1204@gmail.com>
2024-11-22 08:43:32 +0800

    Fetch new url

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -18,7 +18,7 @@ export default function Callback() {
             }
 
             try {
-                const response = await axios.post("https://dongyi-user.hnd1.zeabur.app/callback", {
+                const response = await axios.post("https://dongyi-user.hnd1.zeabur.app/auth/callback", {
                     access_token: code,
                 });
                 const { id_token } = response.data;
