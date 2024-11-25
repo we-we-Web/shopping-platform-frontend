48887d5fc67957819704440080bb4ecf4550dcd6
Ateto1202 <tonysu1204@gmail.com>
2024-11-25 23:06:18 +0800

    Hotfix to css render

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -5,6 +5,8 @@ import Product from '../app/model/product';
 import { useRouter } from 'next/navigation';
 import { SlActionUndo } from 'react-icons/sl';
 import NavigationBar from '../app/component/NavigationBar';
+import '../globals.css';
+import Image from 'next/image';
 
 export default function ProductContent() {
     const [product, setProduct] = useState<Product | null>(null);
@@ -77,23 +79,24 @@ export default function ProductContent() {
     return (
         <div className="flex h-[100%] w-[100%] pt-[10vh]">
             <NavigationBar />
-            <div className="fixed top-10 left-10">
-                <button type="button" onClick={()=>router.back()} className="hover:opacity-70">
-                    <SlActionUndo size={40}/>
-                </button>
-            </div>
-            <div className="flex-col basis-1/2 pl-[15vw]">
-                <img src={product.image || "./default.png"} className="h-96 rounded-lg object-contain"/>
-            </div>
-            <div className="flex-col basis-1/2 pr-[10vw]">
-                <h1 className="text-[4em] font-bold">{product.name}</h1>
-                <div className="text-[3em] text-red-700 font-bold">{product.price} <span className="text-[0.5em]">元</span> </div>
-                <div>{product.description}</div>
-                <button 
-                    className="bg-red-700 w-[10em] h-[2em] text-white mt-[5em] ml-[3em] hover:opacity-60" 
-                    onClick={() => addtoCart(`demo@gmail.com`)}>
-                        add to cart
-                </button>
+            <div className='flex m-8'>
+                <Image 
+                    src={product.image ?? "/default.png"}
+                    className='mr-[5vw] border-gray-400 border object-contain'
+                    alt={product.name}
+                    width={800}
+                    height={800}
+                />
+                <div className="flex-col basis-1/2 pr-[10vw]">
+                    <h1 className="text-[4em] font-bold">{product.name}</h1>
+                    <div className="text-[3em] text-red-700 font-bold">{product.price} <span className="text-[0.5em]">元</span> </div>
+                    <div>{product.description}</div>
+                    <button 
+                        className="bg-red-700 w-[10em] h-[2em] text-white mt-[5em] ml-[3em] hover:opacity-60" 
+                        onClick={() => addtoCart(`demo@gmail.com`)}>
+                            add to cart
+                    </button>
+                </div>
             </div>
         </div>
     );
