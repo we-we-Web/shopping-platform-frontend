de5742e12eea0ea3bc6a1fcabb2dab9e2de0a87d
tyu <225jimmy@gmail.com>
2024-11-29 14:53:50 +0800

    change index page from csr to ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,32 @@
-'use client'
-
-import React, { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 import ProductCard from '../app/component/ProductCard';
 import Product from '../app/model/product';
 import NavigationBar from '../app/component/NavigationBar';
 import '../globals.css';
 
-
-function Home() {
-    const [data, setData] = useState<Product[]>([]);
-    const [isLoading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const url = 'https://dongyi-api.hnd1.zeabur.app/product/products';
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                const result = await response.json();
-                setData(result);
-            } catch (err) {
-                console.error("Error fetching data:", err);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, [data]);
-
-    if (isLoading) {
-        return (
-            <div className="flex justify-center items-center">
-                <h1>
-                    Loading...
-                </h1>
-            </div>
-        )
+interface HomeProps {
+    data: Product[];
+}
+export const getServerSideProps: GetServerSideProps = async () => {
+    try {
+        const url = 'https://dongyi-api.hnd1.zeabur.app/product/products';
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data: Product[] = await response.json();
+        return { props: { data } };
+    } catch (err) {
+        console.error("Error fetching data:", err);
+        return { props: { data: [] } };
     }
+};
 
-    if (!data) {
-        return <div className="p-6">無法獲取資料，請稍後再試。</div>; 
-    }
-
+const Home: React.FC<HomeProps> = ({ data }) => {
     return (
         <>
             <NavigationBar />
-            <div className="p-6 relative mt-16"> 
+            <div className="p-6 relative mt-16">
                 <h1 className="text-2xl font-bold mb-6">商品列表</h1>
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6">
                     {data.map((product, index) => (
@@ -57,6 +36,7 @@ function Home() {
             </div>
         </>
     );
-}
+};
+
 
 export default Home;
